a31c7521e60464e0f42857d496e7a982904bf668
Desenvolvimento <Desenvolvimento@DESKTOP-24SVAOL>
2018-03-23 09:04:19 -0300

    Movendo diretório

---
+++ src/br/com/caelum/estoque/ws/EstoqueWS.java
@@ -0,0 +1,29 @@
+package br.com.caelum.estoque.ws;
+
+import java.util.List;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
+import javax.jws.WebService;
+import javax.xml.ws.RequestWrapper;
+import javax.xml.ws.ResponseWrapper;
+
+import br.com.caelum.estoque.modelo.item.Filtros;
+import br.com.caelum.estoque.modelo.item.Item;
+import br.com.caelum.estoque.modelo.item.ItemDao;
+
+@WebService
+public class EstoqueWS {
+
+	private ItemDao dao = new ItemDao();
+	
+	@WebMethod(operationName="todosOsItens")
+	@WebResult(name="itens")
+	@ResponseWrapper(localName="itens")
+	@RequestWrapper(localName="listaItens")
+	public List<Item> getItens(@WebParam(name="filtros") Filtros filtros) {
+		System.out.println("Chamando getItens");
+		return dao.todosItens();
+	}
+}
